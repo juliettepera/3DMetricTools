46a743441925b808364f3c44fc75ead692f35ba1
Juliette Pera <jpera@eurydice.ia.unc.edu>
2013-12-19 14:33:06 -0500

    ENH: license GPL info

--- 3DMeshMetric/processing.h
+++ 3DMeshMetric/processing.h
@@ -1,3 +1,29 @@
+/* 3DMeshMetric is a visualization tool used to measures and display
+ * surface-to-surface distance between two triangle meshes.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors : Juliette Pera, Francois Budin, Beatriz Paniagua
+ * Web site : http://www.nitrc.org/projects/meshmetric3d/
+ *
+ * Reference : MeshValmet, Validation Metric for Meshes
+ * http://www.nitrc.org/projects/meshvalmet/
+ *
+ * This program also uses Qt libraries
+ * http://www.qt-project.org/
+ */
+
 #ifndef PROCESSING_H
 #define PROCESSING_H
 
