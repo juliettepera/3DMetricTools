4b3dc1efbac0399139766763016c90d455232b6e
Juliette Pera <jpera@hebb.ia.unc.edu>
2014-02-06 15:36:52 -0500

    solved 3DMetricTools.cmake problem

--- 3DMetricTools.cmake
+++ 3DMetricTools.cmake
@@ -2,6 +2,12 @@ cmake_minimum_required(VERSION 2.8.3)
 
 project( 3DMetricTools )
 
+unset( USE_SYSTEM_ITK CACHE )
+unset( USE_SYSTEM_SlicerExecutionModel CACHE )
+unset( USE_SYSTEM_VTK CACHE )
+unset( USE_GIT_PROTOCOL_3DMetricTools_SUPERBUILD CACHE )
+unset( FORCE_EXTERNAL_BUILDS CACHE )
+
 ## A simple macro to set variables ONLY if it has not been set
 ## This is needed when stand-alone packages are combined into
 ## a larger package, and the desired behavior is that all the
@@ -30,35 +36,42 @@ macro(SETIFEMPTY)
   endif(NOT ${KEY})
 endmacro(SETIFEMPTY KEY VALUE)
 endif(NOT SETIFEMPTY)
+
 SETIFEMPTY( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin)
 SETIFEMPTY( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib )
 SETIFEMPTY( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib/static )
-SETIFEMPTY( INSTALL_RUNTIME_DESTINATION 3DMetricTools-package )
-SETIFEMPTY( INSTALL_LIBRARY_DESTINATION ${INSTALL_RUNTIME_DESTINATION}/lib )
 SETIFEMPTY( INSTALL_ARCHIVE_DESTINATION 3DMetricTools-static_lib )
 
-# find the VTK headers
-find_package( VTK REQUIRED )
-if( ${VTK_MAJOR_VERSION} VERSION_LESS 6 )
-  message( FATAL_ERROR "VTK version 6 or higher needed" )
-endif()
-include( ${VTK_USE_FILE} )
-
-# find the SlicerExecutionModel headers
-find_package(SlicerExecutionModel REQUIRED GenerateCLP)
-include( ${SlicerExecutionModel_USE_FILE} )
-include(${GenerateCLP_USE_FILE})
-  
-# find the QT4 headers
-find_package( Qt4 REQUIRED )
-include( ${QT_USE_FILE} )
-
-# set RPATH
-if( UNIX )
-  if(NOT APPLE)
-      SET( CMAKE_INSTALL_RPATH "$ORIGIN/lib" )
+if( ${EXTENSION_NAME}_BUILD_SLICER_EXTENSION )
+  find_package(Subversion REQUIRED )
+  find_package(Git REQUIRED )
+  find_package(Slicer REQUIRED)
+  include(${Slicer_USE_FILE})
+  set( Use_VTK_Filter ON CACHE BOOL "Use VTK Filter" FORCE )
+  set( Build_3DMeshMetric OFF CACHE BOOL "Qt GUI" FORCE )
+  set( INSTALL_RUNTIME_DESTINATION ${SlicerExecutionModel_DEFAULT_CLI_INSTALL_RUNTIME_DESTINATION} )
+  set( INSTALL_LIBRARY_DESTINATION ${SlicerExecutionModel_DEFAULT_CLI_INSTALL_LIBRARY_DESTINATION} )
+else()
+  # find the VTK headers
+  find_package( VTK REQUIRED )
+  if( ${VTK_MAJOR_VERSION} VERSION_LESS 6 )
+    message( FATAL_ERROR "VTK version 6 or higher needed" )
   endif()
+  include( ${VTK_USE_FILE} )
+  # find the SlicerExecutionModel headers
+  find_package(SlicerExecutionModel REQUIRED GenerateCLP)
+  include( ${SlicerExecutionModel_USE_FILE} )
+  include(${GenerateCLP_USE_FILE})
+  # set RPATH
+  if( UNIX )
+    if(NOT APPLE)
+      SET( CMAKE_INSTALL_RPATH "$ORIGIN/lib" )
+    endif()
+  endif()
+  SETIFEMPTY( INSTALL_RUNTIME_DESTINATION 3DMetricTools-package )
+  SETIFEMPTY( INSTALL_LIBRARY_DESTINATION ${INSTALL_RUNTIME_DESTINATION}/lib )
 endif()
+
 
 option( Build_Static "Static libraries and executables only" ON )
 if( Build_Static )
@@ -72,17 +85,33 @@ endif()
 option( Use_VTK_Filter "Use VTK Filter" OFF )
 
 # meshvalmet libraries
-set( MeshValmetLib_SOURCE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Libraries )
-add_subdirectory( Libraries )
+if( Build_3DMeshMetric OR NOT Use_VTK_Filter )
+  # find the QT4 headers
+  find_package( Qt4 REQUIRED )
+  include( ${QT_USE_FILE} )
+  set( MeshValmetLib_SOURCE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Libraries )
+  add_subdirectory( Libraries )
+endif()
 
-option( Build_3DErrorMetric "CLI" ON )
+option( Build_ModelToModelDistance "CLI" ON )
 option( Build_3DMeshMetric "Qt GUI" ON )
 # build errorMetric
-if( Build_3DErrorMetric )
-  add_subdirectory( 3DErrorMetric )
+if( Build_ModelToModelDistance )
+  add_subdirectory( ModelToModelDistance )
 endif()
 
 # build meshMetric
 if( Build_3DMeshMetric )
   add_subdirectory( 3DMeshMetric )
+endif()
+
+option( BUILD_TESTING "Build Testing" ON )
+IF(BUILD_TESTING)
+  include(CTest)
+  ADD_SUBDIRECTORY(Testing)
+ENDIF(BUILD_TESTING)
+
+if( ${EXTENSION_NAME}_BUILD_SLICER_EXTENSION )
+  set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${CMAKE_BINARY_DIR};${EXTENSION_NAME};ALL;/")
+  include(${Slicer_EXTENSION_CPACK})
 endif()
